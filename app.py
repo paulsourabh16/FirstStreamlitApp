e9d5bddec2f635122f709010c62cb7ddc4bbbc39
paulsourabh16 <68888852+paulsourabh16@users.noreply.github.com>
2026-02-03 17:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
+# Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 
-st.title("Hello from Snowflake Streamlit 👋")
+# Write directly to the app
+st.title(f"Sourabhs fruit data entry app :balloon: {st.__version__}")
+st.write(
+  """Enter fruit name and root depth code below.
+  """
+)
+
+# Get the current credentials
+session = get_active_session()
+
+fn = st.text_input('Fruit Name:')
+rdc = st.selectbox('Root Depth:', ('S','M','D'))
+
+if st.button('Submit'):
+    #st.write('Fruit Name entered is: ' + fn)
+    #st.write('Root Depth Code chosen is: ' + rdc)
+    sql_insert = 'insert into garden_plants.fruits.fruit_details select \'' + fn + '\',\'' + rdc + '\''
+    #st.write(sql_insert)
+    result = session.sql(sql_insert)
+    st.write(result)
+
